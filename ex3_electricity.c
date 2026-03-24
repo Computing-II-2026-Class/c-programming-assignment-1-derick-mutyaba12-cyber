d21a1273598462d902775bc6b79e69751e6b1ec7
derick-mutyaba12-cyber <dmutyaba12@gmail.com>
2026-03-24 20:43:29 +0300

    Update ex3_electricity.c
    Assignment 3 completed

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,25 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: MUTYABA DERICK*/
+/* Student Number: 25/U/BIE/01399/PE */
 
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    int units;
+    float costPerUnit, totalBill;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    printf("Enter cost per unit: ");
+    scanf("%f", &costPerUnit);
+
+    totalBill = units * costPerUnit;
+
+    printf("\n----- ELECTRICITY BILL SUMMARY -----\n");
+    printf("Units Consumed : %d\n", units);
+    printf("Cost per Unit  : %.2f UGX\n", costPerUnit);
+    printf("Total Bill     : %.2f UGX\n", totalBill);
+    printf("-----------------------------------\n");
 
     return 0;
 }
